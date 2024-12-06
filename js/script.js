80f9b784c688c429f0674abdd8a19900d82ffcd2
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-06 15:12:51 +0100

    feat: functions lamp

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const turnLampOff = document.querySelector(`Lamp-off`)
 const turnLampOn = document.querySelector(`Lamp-on`)
 const btnOnOff = document.querySelector(`btn`)
-btnOnOff.addEventListener(`click`, function()); {
-    if (turnLampOff.classList.contains)
-}
+// creo una funzione che fa accendere/spegnere la lampada cliccando un bottone
+btnOnOff.addEventListener('click', function() {
+    if (turnLampOff.classList.contains('hidden') {
+        turnLampOff.classList.remove('hidden')
+        turnLampOn.classList.add('hidden'))
+        } else {
+        turnLampOff.classList.add('hidden ')
+        turnLampOn.classList.remove('hidden ')
+        
+        }
+        
+        })
+        
