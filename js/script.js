430087f49fca86ae1e403ae914878459491d82fb
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-06 14:33:08 +0100

    feat: add img and button

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const turnLampOff = document.querySelector(`Lamp-off`)
+const turnLampOn = document.querySelector(`Lamp-on`)
+const btnOnOff = document.querySelector(`btn`)
+btnOnOff.addEventListener(`click`, function()); {
+    if (turnLampOff.classList.contains)
+}
